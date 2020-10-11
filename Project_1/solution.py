736e5d04c46abb03e7886c945ee57da8780be354
CraitorCNC <40051478+CraitorCNC@users.noreply.github.com>
2020-10-11 11:50:14 -0700

    better kernel, unocmmenting approximator for more stability; started adding some nyostroem

--- Project_1/solution.py
+++ Project_1/solution.py
@@ -6,6 +6,8 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel, Matern, WhiteKernel, DotProduct, ExpSineSquared, RationalQuadratic, Sum
 from sklearn.kernel_approximation import Nystroem
 
+#np.random.bit_generator = np.random._bit_generator
+
 ## Constant for Cost function
 THRESHOLD = 0.5
 W1 = 1
@@ -13,47 +15,49 @@ W2 = 20
 W3 = 100
 W4 = 0.04
 
+Npoints = 500
+
 
 def cost_function(true, predicted):
-    """
-        true: true values in 1D numpy array
-        predicted: predicted values in 1D numpy array
+	"""
+		true: true values in 1D numpy array
+		predicted: predicted values in 1D numpy array
 
-        return: float
-    """
-    cost = (true - predicted)**2
+		return: float
+	"""
+	cost = (true - predicted)**2
 
-    # true above threshold (case 1)
-    mask = true > THRESHOLD
-    mask_w1 = np.logical_and(predicted>=true,mask)
-    mask_w2 = np.logical_and(np.logical_and(predicted<true,predicted >=THRESHOLD),mask)
-    mask_w3 = np.logical_and(predicted<THRESHOLD,mask)
+	# true above threshold (case 1)
+	mask = true > THRESHOLD
+	mask_w1 = np.logical_and(predicted>=true,mask)
+	mask_w2 = np.logical_and(np.logical_and(predicted<true,predicted >=THRESHOLD),mask)
+	mask_w3 = np.logical_and(predicted<THRESHOLD,mask)
 
-    cost[mask_w1] = cost[mask_w1]*W1
-    cost[mask_w2] = cost[mask_w2]*W2
-    cost[mask_w3] = cost[mask_w3]*W3
+	cost[mask_w1] = cost[mask_w1]*W1
+	cost[mask_w2] = cost[mask_w2]*W2
+	cost[mask_w3] = cost[mask_w3]*W3
 
-    # true value below threshold (case 2)
-    mask = true <= THRESHOLD
-    mask_w1 = np.logical_and(predicted>true,mask)
-    mask_w2 = np.logical_and(predicted<=true,mask)
+	# true value below threshold (case 2)
+	mask = true <= THRESHOLD
+	mask_w1 = np.logical_and(predicted>true,mask)
+	mask_w2 = np.logical_and(predicted<=true,mask)
 
-    cost[mask_w1] = cost[mask_w1]*W1
-    cost[mask_w2] = cost[mask_w2]*W2
+	cost[mask_w1] = cost[mask_w1]*W1
+	cost[mask_w2] = cost[mask_w2]*W2
 
-    reward = W4*np.logical_and(predicted < THRESHOLD,true<THRESHOLD)
-    if reward is None:
-        reward = 0
-    return np.mean(cost) - np.mean(reward)
+	reward = W4*np.logical_and(predicted < THRESHOLD,true<THRESHOLD)
+	if reward is None:
+		reward = 0
+	return np.mean(cost) - np.mean(reward)
 
 """
 Fill in the methods of the Model. Please do not change the given methods for the checker script to work.
 You can add new methods, and make changes. The checker script performs:
 
 
-    M = Model()
-    M.fit_model(train_x,train_y)
-    prediction = M.predict(test_x)
+	M = Model()
+	M.fit_model(train_x,train_y)
+	prediction = M.predict(test_x)
 
 It uses predictions to compare to the ground truth using the cost_function above.
 """
@@ -61,127 +65,135 @@ It uses predictions to compare to the ground truth using the cost_function above
 
 class Model():
 
-    def __init__(self):
-            
-        self.kernel_ = ConstantKernel() + ConstantKernel()*RBF()
-        self.model = GaussianProcessRegressor(kernel=self.kernel_, n_restarts_optimizer=0,random_state=0)
+	def __init__(self):
+		
+		
+		self.kernel1 = ConstantKernel() + RBF() + WhiteKernel()
+		self.kernel2 = ConstantKernel(constant_value=1.0, constant_value_bounds=(0.0, 10.0)) * RBF(length_scale=0.5, length_scale_bounds=(0.0, 10.0)) + RBF(length_scale=2.0, length_scale_bounds=(0.0, 10.0))
+		self.kernel3 = RationalQuadratic() + WhiteKernel() # Best performes so far
+		self.model = GaussianProcessRegressor(kernel=self.kernel3, n_restarts_optimizer=0, random_state=0)
 
-    def predict(self, test_x):       
-        # predict with model at test point test_x 
+	def predict(self, test_x):       
+		# predict with model at test point test_x 
 
-        y = self.model.predict(test_x)
-        return y
+		y = self.model.predict(test_x)
+		return y
 
-    def load_and_tranform_data(self, approx, train_x, train_y):        
-        data_xy = np.column_stack((train_x,train_y))
-        rng = np.random.default_rng()
+	def load_and_tranform_data(self, approx, train_x, train_y):        
+		
+		data_xy = np.column_stack((train_x,train_y))
+		rng = np.random.default_rng()
 
-        # truncate data set to cut off all points where x1 < -0.5
-        #data_xy = data_xy[data_xy[:,0] >= -0.5, :]        
+		# truncate data set to cut off all points where x1 < -0.5
+		#data_xy = data_xy[data_xy[:,0] >= -0.5, :]        
 
-        # Nyostream approximation (not implemented yet)
-        if(approx == 'Nystroem'):
-            feature_map_nystroem = Nystroem(kernel='rbf',gamma=.2,random_state=1,n_components=300)
-            data_transformed = feature_map_nystroem.fit_transform(train_x)
+		# Nyostream approximation (not implemented yet)
+		if(approx == 'Nystroem'):
+			feature_map_nystroem = Nystroem(kernel='rbf',gamma=.2,random_state=1,n_components=300)
+			data_transformed = feature_map_nystroem.fit_transform(train_x)
 
-        # Select random samples from dataset
-        if(approx == 'Random'):
-            n = 100
-            data_transformed = rng.choice(data_xy,size=n, axis=0, replace=False)            
-        
-        # Clusterize data into 
-        if(approx == 'Clusters'):
-            n_clusters = 20
-            dist_thresehold = 0.07
-            cluster_centers = rng.choice(data_xy,size=n_clusters, axis=0, replace=False)
-            data_transformed = np.zeros((1,3))
-            for i in range(n_clusters):
-                cluster_center = cluster_centers[i,0:3]
-                for point in data_xy:
-                    dist = np.linalg.norm(cluster_center-point)
-                    if(dist < dist_thresehold):
-                        point_app = np.reshape(point,(1,3))
-                        if(data_transformed.all()==0):
-                            data_transformed = point_app
-                            continue
+		# Select random samples from dataset
+		if(approx == 'Random'):
+			n = 100
+			data_transformed = rng.choice(data_xy,size=n, axis=0, replace=False)            
+		
+		# Clusterize data into 
+		if(approx == 'Clusters'):
+			n_clusters = 20
+			dist_thresehold = 0.07
+			cluster_centers = rng.choice(data_xy,size=n_clusters, axis=0, replace=False)
+			data_transformed = np.zeros((1,3))
+			for i in range(n_clusters):
+				cluster_center = cluster_centers[i,0:3]
+				for point in data_xy:
+					dist = np.linalg.norm(cluster_center-point)
+					if(dist < dist_thresehold):
+						point_app = np.reshape(point,(1,3))
+						if(data_transformed.all()==0):
+							data_transformed = point_app
+							continue
 
-                        data_transformed = np.append(data_transformed,point_app,axis=0)
+						data_transformed = np.append(data_transformed,point_app,axis=0)
 
-        # Using entire data set
-        if(approx == 'None'):
-            data_transformed = data_xy
+		# Using entire data set
+		if(approx == 'None'):
+			data_transformed = data_xy
 
-        print("Size of transformed data: " + str(data_transformed.shape))    
-        return data_transformed
+		print("Size of transformed data: " + str(data_transformed.shape))    
+		return data_transformed
 
-    def fit_model(self, train_x, train_y):
+	def fit_model(self, train_x, train_y):
 
-        # load and transform data according to different methods
-        data = self.load_and_tranform_data('Random', train_x, train_y)
-        self.data_x = data[:,0:2]
-        self.data_y = data[:,2]
+		# load and transform data according to different methods
+#       data = self.load_and_tranform_data('Random', train_x, train_y)
+		self.data_x = train_x[0:Npoints,:] #data[:,0:2]
+		self.data_y = train_y[0:Npoints] #data[:,2]
+		
+# 		feature_map_nystroem = Nystroem(gamma=.2, random_state=0, n_components=200)
+# 		data_transformed = feature_map_nystroem.fit_transform(self.data_x)
 
-        # initliaze model with kernel (necessary before actually doing optimization with our custom cost function)
-        self.model.fit(self.data_x, self.data_y)                
+		# initliaze model with kernel (necessary before actually doing optimization with our custom cost function)
+		self.model.fit(self.data_x, self.data_y)                
 
-        # actually optimize hyperparameters according to custom cost function
-        self.model.kernel_.theta = self.optimizer()
+		# actually optimize hyperparameters according to custom cost function --> Why is optimizer after fit function?
+		self.model.kernel_.theta = self.optimizer()
 
-        """ CROSS VALIDATION
-        # Trying different initializartions via cross validation
-        n_restarts=1
-        cost_cv = np.zeros((n_restarts,1))
-        theta = np.zeros((n_restarts, self.model.kernel_.theta.shape[0]))
-        for i in range(n_restarts):
+		""" CROSS VALIDATION
+		# Trying different initializartions via cross validation
+		n_restarts=1
+		cost_cv = np.zeros((n_restarts,1))
+		theta = np.zeros((n_restarts, self.model.kernel_.theta.shape[0]))
+		for i in range(n_restarts):
 
-            # Random parametre initiallization
-            random_initialization = np.random.randn(1,self.model.kernel_.theta.shape[0])[0,:]
-            print('\n',random_initialization)
-            self.model.kernel_.theta = random_initialization
-            self.model.kernel_.theta = self.optimizer()
-            print(self.model.kernel_)
+			# Random parametre initiallization
+			random_initialization = np.random.randn(1,self.model.kernel_.theta.shape[0])[0,:]
+			print('\n',random_initialization)
+			self.model.kernel_.theta = random_initialization
+			self.model.kernel_.theta = self.optimizer()
+			print(self.model.kernel_)
 
-            # Select new cross validation data set
-            d = rng.choice(data_xy,size=200, axis=0, replace=False)
-            x = d[:,0:2]
-            y = d[:,2:3]
-            cost_cv[i] = cost_function(y,self.model.predict(x))
-            theta[i,:] = self.model.kernel_.theta
-            print(cost_cv[i])
-        
-        self.model.kernel_.theta = theta[np.argmin(cost_cv,axis=1)[0],:]
-        """
+			# Select new cross validation data set
+			d = rng.choice(data_xy,size=200, axis=0, replace=False)
+			x = d[:,0:2]
+			y = d[:,2:3]
+			cost_cv[i] = cost_function(y,self.model.predict(x))
+			theta[i,:] = self.model.kernel_.theta
+			print(cost_cv[i])
+		
+		self.model.kernel_.theta = theta[np.argmin(cost_cv,axis=1)[0],:]
+		"""
 
-    def obj_func(self,hyperparams)->float:
-        self.model.kernel_.theta = hyperparams
-        prediction = self.model.predict(self.data_x)
-        cost = cost_function(self.data_y,prediction)
-        self.model.kernel_.theta = hyperparams
+	def obj_func(self,hyperparams)->float:
+		self.model.kernel_.theta = hyperparams
+		prediction = self.model.predict(self.data_x)
+		cost = cost_function(self.data_y,prediction)
+		self.model.kernel_.theta = hyperparams
 
-        return cost
+		return cost
 
-    def optimizer(self):
-        initial_theta = self.model.kernel_.theta
-        optimalResult = scipy.optimize.minimize(self.obj_func, initial_theta, method='BFGS')
-        theta_opt = optimalResult.x
-        return theta_opt
+	def optimizer(self):
+		initial_theta = self.model.kernel_.theta
+		optimalResult = scipy.optimize.minimize(self.obj_func, initial_theta, method='BFGS')
+		theta_opt = optimalResult.x
+		return theta_opt
 
 def main():
-    train_x_name = "train_x.csv"
-    train_y_name = "train_y.csv"
+#   HI there
+	train_x_name = "train_x.csv"
+	train_y_name = "train_y.csv"
 
-    train_x = np.loadtxt(train_x_name, delimiter=',')
-    train_y = np.loadtxt(train_y_name, delimiter=',')
+	train_x = np.loadtxt(train_x_name, delimiter=',')
+	train_y = np.loadtxt(train_y_name, delimiter=',')
 
-    # load the test dateset
-    test_x_name = "test_x.csv"
-    test_x = np.loadtxt(test_x_name, delimiter=',')
+	# load the test dateset
+	test_x_name = "test_x.csv"
+	test_x = np.loadtxt(test_x_name, delimiter=',')
 
-    M = Model()
-    M.fit_model(train_x,train_y)
-    prediction = M.predict(train_x)
-    print("Cost on train_x data: " + str(cost_function(train_y, prediction)))
+	M = Model()
+	M.fit_model(train_x,train_y)
+	prediction = M.predict(train_x)
+	print(f"Cost on train_x data: {cost_function(train_y, prediction)}")
 
 if __name__ == "__main__":
-    main()
-    print("Completed sucessfully")
+	main()
+	print("Completed sucessfully")
