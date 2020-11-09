b01aa48a812b8fc2be2b4dd8884fb2a783e6ece5
Matthijs <mjvanderboon@gmail.com>
2020-11-09 18:48:03 +0100

    cleaned up a bit, regret is somehow different. Unsure why

--- Project 3/solution.py
+++ Project 3/solution.py
@@ -16,9 +16,6 @@ class BO_algo():
     def __init__(self):
         """Initializes the algorithm with a parameter configuration. """
 
-        # TODO: enter your code here
-        #self.File_object = open(r"/Users/adrialopezescoriza/Documents/UNI/GRAU/4A-ETH/PAI/PROJECTS/PAI/Project 3/results.txt","w+")
-
         self.v_min = 1.2
         noise_obs_f = 0.15
         noise_obs_v = 0.0001**2
@@ -36,10 +33,12 @@ class BO_algo():
         self.v = GPR(kernel=self.kernel_v, alpha=1e-10, optimizer='fmin_l_bfgs_b', n_restarts_optimizer=0, normalize_y=False, copy_X_train=True, random_state=None)
 
         # Prealocate data_array
+        self.x_vals = np.zeros([1,1])
+        self.f_vals = np.zeros([1,1])
+        self.v_vals = np.zeros([1,1])
+
         self.data_points = np.zeros([1,3])
         self.data_points_aux = np.zeros([1,5])
-
-        # TODO: finish code
 
 
     def next_recommendation(self):
@@ -52,13 +51,11 @@ class BO_algo():
             1 x domain.shape[0] array containing the next point to evaluate
         """
 
-        # TODO: enter your code here
+        # TODO: optimize giving of next recommendation
         # In implementing this function, you may use optimize_acquisition_function() defined below.
         x_recommended = self.optimize_acquisition_function()
         #x_recommended = domain[:, 0] + (domain[:, 1] - domain[:, 0]) * np.random.rand(domain.shape[0])
         return np.atleast_2d(x_recommended)
-        # TODO: finish code
-
 
     def optimize_acquisition_function(self):
         """
@@ -106,8 +103,7 @@ class BO_algo():
             Value of the acquisition function at x
         """
 
-        # TODO: enter your code here
-
+        # TODO: tune acquisition function
         f_x = self.f.predict(np.atleast_2d(x), return_std=True)
         v_x = self.v.predict(np.atleast_2d(x), return_std=True)
 
@@ -132,9 +128,6 @@ class BO_algo():
 
         return af_value
 
-        # TODO: finish code
-
-
     def add_data_point(self, x, f, v):
         """
         Add data points to the model.
@@ -148,37 +141,18 @@ class BO_algo():
         v: np.ndarray
             Model training speed
         """
+        self.x_vals = np.append(self.x_vals, np.atleast_2d(x))
+        self.f_vals = np.append(self.f_vals, np.atleast_2d(f))
+        self.v_vals = np.append(self.v_vals, np.atleast_2d(v))
 
-        # TODO: enter your code here
+        if (self.x_vals == []):
+            self.x_vals = np.zeros([1,1])
+            self.f_vals = np.zeros([1,1])
+            self.v_vals = np.zeros([1,1])
 
-        # Add data point to data array
-        data_array = np.append(x,np.atleast_2d(f),axis=1)
-        data_array = np.append(data_array,np.atleast_2d(v),axis=1)
-        self.data_points = np.vstack((self.data_points, data_array))
-
-        if(np.sum(self.data_points[0,:]) == 0):
-            self.data_points = np.delete(self.data_points, 0, axis=0)
-            self.data_points_aux = np.delete(self.data_points_aux, 0, axis=0)
-
-        data_array = np.append(x,np.atleast_2d(self.f.predict(x)),axis=1)
-        data_array = np.append(data_array,np.atleast_2d(self.v.predict(x)),axis=1)
-        data_array = np.append(data_array,np.atleast_2d(self.f.predict(x,return_std=True)[1]),axis=1)
-        data_array = np.append(data_array,np.atleast_2d(self.v.predict(x,return_std=True)[1]),axis=1)
-        self.data_points_aux = np.vstack((self.data_points_aux, data_array))
-
-        '''
-        Npoints = 10
-        if(np.shape(self.data_points)[0] > Npoints):
-            data_transformed = KMeans(n_clusters=Npoints, random_state=0).fit(self.data_points[:,0:2]).cluster_centers_
-        else:
-            data_transformed = self.data_points
-        self.f.fit(np.atleast_2d(data_transformed[:,0]).T, np.atleast_2d(data_transformed[:,1]).T)
-        '''
-
-        self.f.fit(np.atleast_2d(self.data_points[:,0]).T, np.atleast_2d(self.data_points[:,1]).T)
-        self.v.fit(np.atleast_2d(self.data_points[:,0]).T, np.atleast_2d(self.data_points[:,2]).T)
-
-        # TODO: finish code
+        # fit surrogate model
+        self.f.fit(np.atleast_2d(self.x_vals).T, np.atleast_2d(self.f_vals).T)
+        self.v.fit(np.atleast_2d(self.x_vals).T, np.atleast_2d(self.v_vals).T)
 
     def get_solution(self):
         """
@@ -193,32 +167,24 @@ class BO_algo():
         # TODO: enter your code here
         
         # minimum speed recquired
-        mask = (self.data_points[:, 2] > self.v_min)
-        suitable_points = self.data_points[mask]
-        try:
-            #h = hello
-            opt_idx = np.argmax(suitable_points[:,1])
-        except:
-            opt_idx = np.argmax(self.data_points[:,1])
-            suitable_points = self.data_points
+        mask = self.v_vals > self.v_min
 
+        if (np.all(mask == False)): # if v is nowhere larger than v_min, just use all points
+            mask = [True for _ in self.v_vals]
 
-        # Comments to check out how everything is going
-        print("Real Points:\n", self.data_points)
-        print("\nEstimated Points:\n", self.data_points_aux[:,:])
-        print("Selected Point:")
-        print(suitable_points[opt_idx])
-        print(self.acquisition_function(suitable_points[opt_idx,0]), self.f.predict(np.atleast_2d(suitable_points[opt_idx,0]), return_std=True))
-        print("F Regression Score:", self.f.score(np.atleast_2d(self.data_points[:,0]).T, np.atleast_2d(self.data_points[:,1]).T))
-        print("V Regression Score:", self.v.score(np.atleast_2d(self.data_points[:,0]).T, np.atleast_2d(self.data_points[:,2]).T))
+        opt_idx = np.argmax(self.f_vals[mask])
 
-        
+        # printouts
+        print("test points (x): ", self.x_vals)
+        print("selected point: ", self.x_vals[opt_idx], ", f: ", self.f_vals[opt_idx])
+        print("acquisition function: ", self.acquisition_function(self.x_vals[opt_idx]))
+        print("F regression score: ", self.f.score(np.atleast_2d(self.x_vals).T, np.atleast_2d(self.f_vals).T))
+        print("V regression score: ", self.v.score(np.atleast_2d(self.x_vals).T, np.atleast_2d(self.v_vals).T))
+
         #self.File_object.write("Real Points:\n" + str(self.data_points) + "\nEstimated Points:\n" +  str(self.data_points_aux[:,:]) + "\nSelected Point:" + str(suitable_points[opt_idx]))
         #File_object.close()
         #time.sleep(5000)
-        return suitable_points[opt_idx, 0]
-
-        # TODO: finish code
+        return self.x_vals[opt_idx]
 
 """ Toy problem to check code works as expected """
 
